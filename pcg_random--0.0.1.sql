e16b96b478c1bd6e893428981bede0de59098f3f
cayetanobv <cayetanobv@gmail.com>
2016-09-18 01:51:41 +0200

    sql script and control file

---
+++ pcg_random--0.0.1.sql
@@ -0,0 +1,7 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pcg_random" to load this file. \quit
+
+CREATE FUNCTION pcg_random(integer)
+RETURNS integer
+AS 'MODULE_PATHNAME'
+LANGUAGE C IMMUTABLE STRICT;
