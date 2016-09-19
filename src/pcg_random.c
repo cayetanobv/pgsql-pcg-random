91e63a244a06b3d2f6a6ca7e5857eb137533e75e
cayetanobv <cayetanobv@gmail.com>
2016-09-19 02:24:45 +0200

    new functions returns array type

--- src/pcg_random.c
+++ src/pcg_random.c
@@ -28,9 +28,11 @@
 
 #include <postgres.h>
 #include <fmgr.h>
-#include <utils/numeric.h>
+#include <utils/array.h>
+#include <catalog/pg_type.h>
 
 #include "pcg-c-basic/pcg_basic.h"
+
 
 PG_MODULE_MAGIC;
 
@@ -72,4 +74,62 @@ pcg_random_bound(PG_FUNCTION_ARGS)
     result = pcg32_boundedrand_r(&rng, bound);
 
     PG_RETURN_UINT32(result);
+}
+
+
+PG_FUNCTION_INFO_V1(pcg_random_array);
+Datum
+pcg_random_array(PG_FUNCTION_ARGS)
+{
+
+    ArrayType *result;
+    Datum  *arr_data;
+    int32 seedinitseq;
+    int32 arr_sz;
+    pcg32_random_t rng;
+
+    arr_sz = PG_GETARG_INT32(0);
+    seedinitseq = PG_GETARG_INT32(1);
+
+    arr_data = (Datum *) palloc(sizeof(Datum) * arr_sz);
+
+    pcg32_srandom_r(&rng, time(NULL) ^ (intptr_t)&printf,(intptr_t)&seedinitseq);
+
+    for ( int i=0; i<arr_sz; i++ ){
+        arr_data[i] = pcg32_random_r(&rng);
+    }
+
+    result = construct_array(arr_data, arr_sz, INT4OID, sizeof(uint32_t), true, 'i');
+
+    PG_RETURN_ARRAYTYPE_P(result);
+}
+
+
+PG_FUNCTION_INFO_V1(pcg_random_bound_array);
+Datum
+pcg_random_bound_array(PG_FUNCTION_ARGS)
+{
+
+    ArrayType *result;
+    Datum  *arr_data;
+    int32 arr_sz;
+    int32 seedinitseq;
+    uint32_t bound;
+    pcg32_random_t rng;
+
+    arr_sz = PG_GETARG_INT32(0);
+    bound = PG_GETARG_INT32(1);
+    seedinitseq = PG_GETARG_INT32(2);
+
+    arr_data = (Datum *) palloc(sizeof(Datum) * arr_sz);
+
+    pcg32_srandom_r(&rng, time(NULL) ^ (intptr_t)&printf,(intptr_t)&seedinitseq);
+
+    for ( int i=0; i<arr_sz; i++ ){
+        arr_data[i] = pcg32_boundedrand_r(&rng, bound);
+    }
+
+    result = construct_array(arr_data, arr_sz, INT4OID, sizeof(uint32_t), true, 'i');
+
+    PG_RETURN_ARRAYTYPE_P(result);
 }
